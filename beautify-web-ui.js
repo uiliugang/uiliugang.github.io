8b62b785972ea1ed7cc0089280bbc455b3b79771
Newgun <74343774+uiliugang@users.noreply.github.com>
2024-03-12 22:30:18 +0800

    updated

--- beautify-web-ui.js
+++ beautify-web-ui.js
@@ -73,7 +73,7 @@
     .autboP_xS3EJZt4GoTeY {
         display: none !important;
     }
-    .prompt-container, .el-aside, #dialog-0, .semi-modal-mask {
+    .prompt-container, .el-aside, #dialog-0, .semi-modal-mask, .semi-portal {
      display: none !important;
     }
     `;
